817992db60aca2d0b7cb7ceac1ba687729ad678f
WraasTheOne <nicholas.mazur.hansen@gmail.com>
2024-12-10 15:54:02 +0100

    made data ingestion

--- dataingetstion/internal/auth/auth.go
+++ dataingetstion/internal/auth/auth.go
@@ -2,20 +2,48 @@ package auth
 
 import (
 	"Public-automated-check-in/dataingestion/proto"
+	"context"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 )
 
 // make grpc call for auth service using roken from incomming request
-func AuthClient(c *gin.Context) bool {
-	// Set up a connection to the server.
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+func AuthMiddleware(c *gin.Context) {
+	token := c.GetHeader("Authorization")
+	if token == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization token is required"})
+		c.Abort()
+		return
 	}
 
-	client := proto.NewAuthServiceClient(conn)
+	// Call gRPC AuthService
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Printf("Failed to connect to AuthService: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to AuthService"})
+		c.Abort()
+		return
+	}
+	defer conn.Close()
 
+	client := proto.NewAuthServiceClient(conn)
+	response, err := client.AuthClient(context.Background(), &proto.AuthClientRequest{Token: token})
+	if err != nil {
+		log.Printf("Failed to authenticate token: %v", err)
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+		c.Abort()
+		return
+	}
+
+	if !response.AuthStatus {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": response.Message})
+		c.Abort()
+		return
+	}
+
+	// Continue if authenticated
+	c.Next()
 }
--- dataingetstion/internal/redisdb/redisdb.go
+++ dataingetstion/internal/redisdb/redisdb.go
@@ -1,1 +1,35 @@
 package redisdb
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+var rdb *redis.Client
+
+// InitRedis initializes the Redis client
+func InitRedis() error {
+	rdb = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379", // Update if Redis is not on localhost
+	})
+	_, err := rdb.Ping().Result()
+	return err
+}
+
+// CloseRedis closes the Redis connection
+func CloseRedis() {
+	rdb.Close()
+}
+
+// AddToStream adds a message to the Redis stream
+func AddToStream(streamName string, data map[string]interface{}) error {
+	err := rdb.XAdd(&redis.XAddArgs{
+		Stream: streamName,
+		Values: data,
+	}).Err()
+	if err != nil {
+		return fmt.Errorf("failed to add data to Redis stream: %v", err)
+	}
+	return nil
+}
